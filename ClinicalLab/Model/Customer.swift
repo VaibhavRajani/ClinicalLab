011237d4b86409842d4190221e5b901f9a8302fc
Vaibhav Rajani <101408581+VaibhavRajani@users.noreply.github.com>
2024-03-28 01:33:28 -0400

    Restructuring models

---
+++ ClinicalLab/Model/Customer.swift
@@ -0,0 +1,8 @@
+//
+//  Customer.swift
+//  ClinicalLab
+//
+//  Created by Vaibhav Rajani on 3/28/24.
+//
+
+import Foundation
---
+++ ClinicalLab/Model/Vehicle.swift
@@ -0,0 +1,8 @@
+//
+//  Vehicle.swift
+//  ClinicalLab
+//
+//  Created by Vaibhav Rajani on 3/28/24.
+//
+
+import Foundation
